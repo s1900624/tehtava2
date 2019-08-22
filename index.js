346ebbf81428fae2dfa82d339fe5cba013f0f684
Ilmast Toivo <s1900624@edu.bc.fi>
2019-08-22 10:14:36 +0300

    Add: function runner

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 window.onload = function() {
+    const runnerImgs = ['runner1.png', 'runner2.png', 'runner3.png', 'runner4.png'];
+
+    const runnerImageId = document.getElementById('runner-image');
+
+    let count = 0;
+    const runner = () => {
+        setTimeout( () => {
+            runnerImageId.src = `./imgs/${runnerImgs[count]}`;
+            count = count < 4 ? count += 1 : 0;
+            runner();
+        }, 60);
+    }
+    runner();
 };

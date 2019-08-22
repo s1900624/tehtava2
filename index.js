ed90b8688fe858e46ba643d3f8acc9b27930907f
Ilmast Toivo <s1900624@edu.bc.fi>
2019-08-22 10:29:52 +0300

    Add: button ´stop´

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ window.onload = function() {
     const runnerImgs = ['runner1.png', 'runner2.png', 'runner3.png', 'runner4.png'];
 
     const runnerImageId = document.getElementById('runner-image');
+    const buttonStop = document.getElementById('stop');
 
     let count = 0;
+    let runnerId = 0;
     const runner = () => {
-        setTimeout( () => {
+        runnerId = setTimeout( () => {
             runnerImageId.src = `./imgs/${runnerImgs[count]}`;
-            count = count < 4 ? count += 1 : 0;
+            count = count < 3 ? count += 1 : 0;
             runner();
         }, 60);
     }
+
     runner();
+    buttonStop.onclick = () => clearTimeout(runnerId);
 };
